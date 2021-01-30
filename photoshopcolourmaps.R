c94cd3af51999c79c11ff602d4001eec7623cda5
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2021-01-30 12:47:01 +0100

    Add files via upload

--- photoshopcolourmaps.R
+++ photoshopcolourmaps.R
@@ -7,17 +7,53 @@ library(viridis)
 
 
 # Auxiliary write binary functions
-escribir_bin=function(valor, fichero) {
+writeValue=function(valor, fichero) {
     writeBin(0L, fichero, size=1)
     writeBin(as.integer(round(valor)), fichero, size=1)
 }
 
-escribir_point=function(x, y, fichero) {
+writePoint=function(x, y, fichero) {
     writeBin(0L, fichero, size=1)
     writeBin(as.integer(round(y)), fichero, size=1)
     writeBin(0L, fichero, size=1)
     writeBin(as.integer(round(x)), fichero, size=1)
 }
+
+# Function that creates a Photoshop ACV curve file
+writePhotoshopACVcurve=function(colourmap, nombreacv) {
+    # Write values in ACV curves:
+    # https://www.adobe.com/devnet-apps/photoshop/fileformatashtml/#50577411_pgfId-1056330
+    # Count of points in the curve (short integer from 2...19) -> NO 19 SINO 16
+    # Curve points. Each curve point is a pair of short integers
+    # where the first number is the output value (vertical coordinate on
+    # the Curves dialog graph) and the second is the input value.
+    # All coordinates have range 0 to 255
+    
+    # Build ACV file
+    acv=file(nombreacv, 'wb')
+    
+    # Write header
+    ACVVERSION=4  # version 4 = count of curves in the file
+    NCURVES=4  # count of curves in the file: RGB, R, G and B
+    writeValue(ACVVERSION, acv)  # ACV version
+    writeValue(NCURVES, acv)  # count of curves
+    
+    # RGB curve (NULL)
+    writeValue(2, acv)  # 2 points in curve
+    writePoint(0, 0, acv)  # (0,0)
+    writePoint(255, 255, acv)  # (255,255)
+    
+    # R, G and B curves
+    for (curve in 1:3) {
+        writeValue(NCOL, acv)  # NCOL points in curve
+        for (j in 1:NCOL) {
+            writePoint(round(255/(NCOL-1)*(j-1)), colourmap[j,curve], acv)
+        }
+    }
+    
+    close(acv)
+}
+
 
 # Matlab 'Jet' colour palette
 jet.colors=colorRampPalette(
@@ -47,7 +83,7 @@ for (i in 1:NCOLMAPS) {  # loop through all colourmaps
     # Subsample 16 RGB values (pick 1 sample every WIDTH samples)
     colourmap=colourmap[(row(colourmap)-1) %%  WIDTH == 0]
     dim(colourmap)=c(NCOL,3)
-
+    
     # NOTE1: doing just colourmap=t(col2rgb(cmfun[[i]](NCOL))) with NCOL=16
     # leads to offset errors in all built-in colourmaps (viridis are fine)
     # hence we prefer to subsample from NCOL_ORG=256 
@@ -68,36 +104,7 @@ for (i in 1:NCOLMAPS) {  # loop through all colourmaps
     abline(h=c(0,1), v=c(0,1), col='gray', lty='dotted')
     dev.off()
     
-    
-    # Write values in ACV curves:
-    # https://www.adobe.com/devnet-apps/photoshop/fileformatashtml/#50577411_pgfId-1056330
-    # Count of points in the curve (short integer from 2...19) -> NO 19 SINO 16
-    # Curve points. Each curve point is a pair of short integers
-    # where the first number is the output value (vertical coordinate on
-    # the Curves dialog graph) and the second is the input value.
-    # All coordinates have range 0 to 255
+    # Write Photoshop ACV curve file
+    writePhotoshopACVcurve(colourmap, nombreacv=paste0(cmname[i],'.acv'))
 
-    # Build ACV file
-    acv=file(paste0(cmname[i],'.acv'), 'wb')
-    
-    # Write header
-    ACVVERSION=4  # version 4 = count of curves in the file
-    NCURVES=4  # count of curves in the file: RGB, R, G and B
-    escribir_bin(ACVVERSION, acv)  # ACV version
-    escribir_bin(NCURVES, acv)  # count of curves
-    
-    # RGB curve (NULL)
-    escribir_bin(2, acv)  # 2 points in curve
-    escribir_point(0, 0, acv)  # (0,0)
-    escribir_point(255, 255, acv)  # (255,255)
-    
-    # R, G and B curves
-    for (curve in 1:3) {
-        escribir_bin(NCOL, acv)  # NCOL points in curve
-        for (j in 1:NCOL) {
-            escribir_point(round(255/(NCOL-1)*(j-1)), colourmap[j,curve], acv)
-        }
-    }
-    
-    close(acv)
 }
